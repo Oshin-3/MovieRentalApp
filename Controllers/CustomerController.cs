a60bb7e97273af86270dedeb181ee71ede2c4d27
Oshin-3 <oshin.gawande3@gmail.com>
2024-11-22 18:57:56 +0530

    added customers and movies details

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRentalApp.Models;
+using MovieRentalApp.ViewModel;
+
+namespace MovieRentalApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        
+        public CustomerController() { 
+            
+        }
+        // GET: Customer
+        public ActionResult Customers()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int? id) 
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null) {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        private IEnumerable<Customers> GetCustomers()
+        {
+            return new List<Customers> {
+                 new Customers { Id = 1, Name = "Dheeraj" },
+                new Customers { Id = 2, Name = "Sunny" },
+                new Customers { Id = 3, Name = "Oshin" }
+            };
+        }
+    }
+}
--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using MovieRentalApp.Models;
-using MovieRentalApp.ViewModel;
+
 
 namespace MovieRentalApp.Controllers
 {
@@ -13,52 +13,79 @@ namespace MovieRentalApp.Controllers
     {
         // GET: Movie
        
-        public ActionResult Random()
-        {
-            var movies = new Movies() { Title = "Dune 2!" };
+        //public ActionResult Random()
+        //{
+        //    var movies = new Movies() { Title = "Dune 2!" };
 
-            var customers = new List<Customers>
-            { 
-                new Customers { Name = "Customer 1"},
-                new Customers { Name = "Customer 2"}
+        //    var customers = new List<Customers>
+        //    { 
+        //        new Customers { Name = "Customer 1"},
+        //        new Customers { Name = "Customer 2"}
             
-            };
+        //    };
 
-            var randomMovie = new RandomViewModel()
-            {
-                Movies = movies,
-                Customers = customers
-            };
+        //    var randomMovie = new RandomViewModel()
+        //    {
+        //        Movies = movies,
+        //        Customers = customers
+        //    };
 
-            return View(randomMovie);
-        }
+        //    return View(randomMovie);
+        //}
 
-        //Action parameters
-        //1. URL and 2. Query string
-        public ActionResult Edit(int id) { 
+        ////Action parameters
+        ////1. URL and 2. Query string
+        //public ActionResult Edit(int id) { 
             
-            return Content("Id from the URL : " +  id);
-        }
+        //    return Content("Id from the URL : " +  id);
+        //}
 
-        public ActionResult Index(int? pageNo, string sortBy)
+        //public ActionResult Index(int? pageNo, string sortBy)
+        //{
+        //    if (!pageNo.HasValue)
+        //    {
+        //        pageNo = 1;
+        //    }
+
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
+
+        //    return Content(String.Format("pageNo={0}&sortBy={1}", pageNo, sortBy));
+        //}
+
+        //[Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        //public ActionResult ByReleasedDate(int year, int month) 
+        //{ 
+        //    return Content(String.Format("Released Date : {0}/{1}", year, month));
+        //}
+
+        public ActionResult Movies()
         {
-            if (!pageNo.HasValue)
-            {
-                pageNo = 1;
-            }
-
-            if (string.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageNo={0}&sortBy={1}", pageNo, sortBy));
+            var movies = GetMovies();
+            return View(movies);
         }
 
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
-        public ActionResult ByReleasedDate(int year, int month) 
-        { 
-            return Content(String.Format("Released Date : {0}/{1}", year, month));
+        public ActionResult Details(int? id) 
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            else { 
+                return View(movie);
+            }
+        }
+
+        private IEnumerable<Movies> GetMovies()
+        {
+            return new List<Movies>
+            {
+                new Movies { Id = 100, Title = "Dune 2"},
+                new Movies { Id = 101, Title = "Wicked"}
+            };
         }
     }
 }
